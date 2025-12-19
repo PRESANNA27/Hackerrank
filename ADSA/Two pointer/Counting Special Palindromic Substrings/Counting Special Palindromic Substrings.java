ccd2394b942e99fe44287194bdf997af7998dced
PRESANNAKUMAR S <spresannakumarpvt27@gmail.com>
2025-12-19 23:14:23 +0530

    Refactor palindrome counting logic in main class

--- ADSA/Two pointer/Counting Special Palindromic Substrings/Counting Special Palindromic Substrings.java
+++ ADSA/Two pointer/Counting Special Palindromic Substrings/Counting Special Palindromic Substrings.java
@@ -1,40 +1,30 @@
 import java.util.*;
 
-public class PalindromicSubstrings {
-    
-    public static int countPalindromes(String s) {
+public class Main {
+
+    static int expand(String s, int left, int right) {
         int count = 0;
-        int n = s.length();
 
-        for (int i = 0; i < n; i++) {
-            // Odd length palindromes
-            count += expandFromCenter(s, i, i);
-
-            // Even length palindromes
-            count += expandFromCenter(s, i, i + 1);
-        }
-
-        return count;
-    }
-
-    private static int expandFromCenter(String s, int left, int right) {
-        int cnt = 0;
-
-        while (left >= 0 && right < s.length()
-                && s.charAt(left) == s.charAt(right)) {
-            cnt++;
+        while (left >= 0 && right < s.length() &&
+               s.charAt(left) == s.charAt(right)) {
+            count++;
             left--;
             right++;
         }
-
-        return cnt;
+        return count;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
 
-        System.out.println(countPalindromes(s));
-        sc.close();
+        int ans = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            ans += expand(s, i, i);     // odd length
+            ans += expand(s, i, i + 1); // even length
+        }
+
+        System.out.println(ans);
     }
 }
